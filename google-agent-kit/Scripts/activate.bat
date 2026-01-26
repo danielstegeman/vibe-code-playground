bb0fa3fb2c767f6ec3aabd97b6e512ea9d68e100
Daniel Stegeman <daniel.stegeman@infosupport.com>
2026-01-26 18:39:49 +0100

    py env

--- google-agent-kit/Scripts/activate.bat
+++ google-agent-kit/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\danielst\ai playground\vibe-code-playground\google-agent-kit"
+set "VIRTUAL_ENV=C:\Users\danielst\source\vibe-code-playground\google-agent-kit"
 
 if not defined PROMPT set PROMPT=$P$G
 
